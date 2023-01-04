f6c7f26661844e72e230fc91f52e563bd28d9827
kokobotz <121683028+kokobotz@users.noreply.github.com>
2023-01-04 11:25:56 +0700

    Add files via upload

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -80,8 +80,8 @@ let handler = async (m, { conn }) => {
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m) => {
-m.reply(`*BOT ON 24 JAM*
-SEWA BOT 1 BULAN 10K`)
+m.reply(`10/30 HR, 5K/7HR
+`)
 }
 
 handler.help = ['sewa']
